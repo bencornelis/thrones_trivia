e3566acd1a042d36929b8a80b221d40c9f658bd3
Ben Cornelis <bencornelis02@gmail.com>
2017-09-21 11:16:07 -0700

    remove unused answer_class ResponseHelper

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -1,7 +1,2 @@
 module ResponsesHelper
-  def answer_class(question, response, answer)
-    return 'list-group-item-success' if question.correct_answer == answer
-    return 'list-group-item-danger'  if response.answer == answer
-    ""
-  end
 end
